b57ae4d9b1ab8e616f0e29a3f95e452bd6f468c6
rugwall01 <161780368+Rugwall01@users.noreply.github.com>
2025-01-18 21:13:28 -0800

    changed player name and marker objects to getter methods, as well as the returned gameboard object to a getter method, Changed GameBoard from and array to an object itself with Array.fill().map(()=>array.fill(). Removed globalcontrols and declared movesMade and getCurrentPlayer 'globally' within the GameController closure, changed nextMove to retain prompt return valu inside move and check the value of the input against trings and numbers outside of the range 1-9, replaced PLaceMarker with playMove, defined placeMarker inside of new GameBoard object.

--- script.js
+++ script.js
@@ -48,106 +48,66 @@ const player2 = (function () {
         getMarker: () => marker
     };
 })();
-
-
 const GameBoard = (function () {
-    const columns = 3;
-    const rows = 3;
-    const gameBoard = [];
-    for(let i = 0; i < columns; i++) {
-        let column = [];
-        for(let j = 0; j < rows; j++) {
-            column.push(0);
-        }
-        gameBoard.push(column)
-    }
-    return gameBoard;
+    const board = Array(3).fill(null).map(() => Array(3).fill(0));
 
+    const getBoard = () => board;
+    const placeMarker = (row, col, marker) => {
+        if (board[row][col] === 0) {
+            board[row][col] = marker;
+            return true;
+        }
+        return false;
+    };
+    return { getBoard, placeMarker };
 })();
 
 
 const GameController = (function () {
+    let movesMade = 0;
 
-    const globalControls = (function () {
-        let movesMade = 0;
-        const addMove = function () {
-            movesMade++;
-        }
-        const getMoves = function () {
-            return movesMade;
-        }
-        return {addMove, getMoves};
+    const getCurrentPlayer = () => (movesMade % 2 === 0 ? player1 : player2);
 
-    })();
-
-
-    const Player = function() {
-        return (globalControls.getMoves() % 2 === 0) ? player1 : player2;
-
-    };
-
-
-    const PlaceMarker = function (cell) {
-
-        const row = Math.floor((cell - 1) / 3);
-        const col = (cell - 1) % 3;
-
-
-        const place = function () {
-            if (GameBoard[row][col] === 0) {
-            GameBoard[row][col] = Player().marker;
-            console.log(`Placed ${Player().marker} at (${row}, ${col})`);
-            }else { 
-            console.log("Can't place there! That spot is already taken.");
-            
-            };
-        };
-        return {place};
-        
-
-    };
-
-    const NextMove = function () {
-
-        let nextMove;
+    const nextMove = () => {
+        let move;
         while (true) {
-            nextMove = Number(prompt("Enter a grid cell (1-9): "));
-            if (!isNaN(nextMove) && nextMove >= 1 && nextMove <= 9) break;
+            move = Number(prompt("Enter a grid cell (1-9): "));
+            if (!isNaN(move) && move >= 1 && move <= 9) break;
             console.log("Invalid input. Please enter a number between 1 and 9.");
         }
-        
-        return {nextMove};
-    }
+        return move;
+    };
 
-    return {globalControls, Player, PlaceMarker, NextMove};
+    const playMove = (cell) => {
+        const row = Math.floor((cell - 1) / 3);
+        const col = (cell - 1) % 3;
+        const currentPlayer = getCurrentPlayer();
 
+        if (GameBoard.placeMarker(row, col, currentPlayer.getMarker())) {
+            console.log(`Placed ${currentPlayer.getMarker()} at (${row}, ${col})`);
+            movesMade++;
+        } else {
+            console.log("Can't place there! That spot is already taken.");
+        }
+    };
+
+    return { nextMove, playMove, getCurrentPlayer };
 })();
 
-
 const GameFlow = function () {
-
     console.log("Welcome to Tic-Tac-Toe!");
     console.log("Player 1 (X) goes first. Enter a number (1-9) to place your marker.");
-    
-   
 
-    for (let i = 0; i < 9; i++){
-    let nextMove = GameController.NextMove();
-    GameController.PlaceMarker(nextMove.nextMove).place();
-    GameController.globalControls.addMove();
-    console.table(GameBoard);
-    
-    };
-    console.log("Game over!")
-    
+    for (let i = 0; i < 9; i++) {
+        
+        console.log(`Current Turn: ${GameController.getCurrentPlayer().getName()}`);
+        let move = GameController.nextMove();
+        GameController.playMove(move);
+        console.table(GameBoard.getBoard());        
+    }
 
+    console.log("Game over!");
 };
 
-
-
-
-// gridCell.addEventListener("click", (e) => {
-//     GameController.PlaceMarker(e.target).place;
-// });
-
 GameFlow();
+
